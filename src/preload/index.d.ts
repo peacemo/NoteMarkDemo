31f5807e17643d4e27c482327eb50006e7f480b0
peacemo <carlchenxq@qq.com>
2025-01-03 16:02:58 +0800

    chore: Initialize configs, start with a demo "hello"

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,8 +1,6 @@
-import { ElectronAPI } from '@electron-toolkit/preload'
-
 declare global {
   interface Window {
-    electron: ElectronAPI
-    api: unknown
+    // electron: ElectronAPI
+    context: {} // define the context in the index.ts (same type).
   }
 }
