2bbbcc98a5fe04dd84e18185ceb23b6a078eab95
Chen Xueqiang <carlchenxq@qq.com>
2025-01-04 00:10:09 +0800

    feat: Change some color schemes

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -12,11 +12,11 @@ const App = () => {
     <>
       <DraggableTopBar />
       <RootLayout>
-        <Sidebar className="p-2 bg-zinc-700">
+        <Sidebar className="p-4 bg-zinc-700">
           <ActionButtonsRow className="flex justify-between mt-8" />
           <NotePreviewList className="mt-3 space-y-1" />
         </Sidebar>
-        <Content className="border-l bg-zinc-900 border-l-white/20">Contents</Content>
+        <Content className="border-l bg-zinc-900/90 border-l-white/20 pt-8 pl-5">Content</Content>
       </RootLayout>
     </>
   )
