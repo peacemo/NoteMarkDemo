4349da5e50d0b39a64f8ec5ad23eab782283b3ee
peacemo <carlchenxq@qq.com>
2025-01-11 00:48:03 +0800

    feat: change the floating title and content on sidebar click

--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -10,12 +10,12 @@ export const selectedNoteAtom = atom((get) => {
   const notes = get(notesAtom)
   const selectedNoteIndex = get(selectedNoteIndexAtom)
 
-  if (!selectedNoteIndex) return null
+  if (selectedNoteIndex == null) return null
 
   const selectedNote = notes[selectedNoteIndex]
 
   return {
     ...selectedNote,
-    content: `Hello from Note ${selectedNoteIndex}`
+    content: `Hello from Note ${selectedNoteIndex + 1}`
   }
 })
