f23647687a524950306ae66390aedb717c8f0b15
peacemo <carlchenxq@qq.com>
2025-01-11 20:01:06 +0800

    feat: make creat and delete button functional (react FE)

--- src/renderer/src/components/Button/DeleteNoteButton.tsx
+++ src/renderer/src/components/Button/DeleteNoteButton.tsx
@@ -1,9 +1,17 @@
 import { ActionButton, ActionButtonProps } from '@/components'
+import { deleteNoteAtom } from '@renderer/store'
+import { useSetAtom } from 'jotai'
 import { LuTrash2 } from 'react-icons/lu'
 
 export const DeleteNoteButton = ({ ...props }: ActionButtonProps) => {
+  const deleteNote = useSetAtom(deleteNoteAtom)
+
+  const handleDelete = () => {
+    deleteNote()
+  }
+
   return (
-    <ActionButton {...props}>
+    <ActionButton onClick={handleDelete} {...props}>
       <LuTrash2 className="w-4 h-4 text-zinc-300" />
     </ActionButton>
   )
--- src/renderer/src/components/Button/NewNoteButton.tsx
+++ src/renderer/src/components/Button/NewNoteButton.tsx
@@ -1,9 +1,17 @@
 import { ActionButton, ActionButtonProps } from '@/components'
+import { creatEmptyNoteAtom } from '@renderer/store'
+import { useSetAtom } from 'jotai'
 import { LuFilePenLine } from 'react-icons/lu'
 
 export const NewNoteButton = ({ ...props }: ActionButtonProps) => {
+  const createEmptyNote = useSetAtom(creatEmptyNoteAtom)
+
+  const handlCreation = () => {
+    createEmptyNote()
+  }
+
   return (
-    <ActionButton {...props}>
+    <ActionButton onClick={handlCreation} {...props}>
       <LuFilePenLine className="w-4 h-4 text-zinc-300" />
     </ActionButton>
   )
